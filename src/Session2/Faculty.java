10a9afb626a561e7edcd2bb4aa6358d252a97b21
Sajjad Pishvaeian <pishvaeian@aut.ac.ir>
2020-10-11 22:43:29 +0330

    javadoc added to faculty class.

--- src/Session2/Faculty.java
+++ src/Session2/Faculty.java
@@ -7,47 +7,78 @@ public class Faculty {
     private String name;
     private int labNum;
 
+    /**
+     * Create a new faculty with a name and lab number.
+     *
+     * @param name name of Faculty
+     * @param labNum number of Labs
+     */
     public Faculty(String name, int labNum) {
         this.name = name;
         this.labNum = labNum;
     }
-
+    /**
+     * set labs of faculty
+     */
     public void setLabs(Lab[] labs) {
         this.labs = labs;
     }
-
+    /**
+     * set students of faculty
+     */
     public void setStudents(Student[] students) {
         this.students = students;
     }
-
+    /**
+     * set master students of faculty
+     */
     public void setMaster(Student master) {
         this.master = master;
     }
-
+    /**
+     * set the name of faculty
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * set the lab number
+     */
     public void setLabNum(int labNum) {
         this.labNum = labNum;
     }
-
+    /**
+     * get the labs of faculty
+     * @return labs field
+     */
     public Lab[] getLabs() {
         return labs;
     }
-
+    /**
+     * get the students of faculty
+     * @return students field
+     */
     public Student[] getStudents() {
         return students;
     }
-
+    /**
+     * get the master students of faculty
+     * @return master field
+     */
     public Student getMaster() {
         return master;
     }
-
+    /**
+     * get the name of faculty
+     * @return name field
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * get the lab number in faculty
+     * @return labNum field
+     */
     public int getLabNum() {
         return labNum;
     }
