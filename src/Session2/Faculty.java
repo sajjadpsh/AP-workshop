655f923fe2ec37433cb54614ebef12552a1a2bdc
Sajjad Pishvaeian <pishvaeian@aut.ac.ir>
2020-10-11 21:03:20 +0330

    Faculty class created. methods added to this class

--- src/Session2/Faculty.java
+++ src/Session2/Faculty.java
@@ -6,4 +6,49 @@ public class Faculty {
     private Student master;
     private String name;
     private int labNum;
+
+    public Faculty(String name, int labNum) {
+        this.name = name;
+        this.labNum = labNum;
+    }
+
+    public void setLabs(Lab[] labs) {
+        this.labs = labs;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public void setMaster(Student master) {
+        this.master = master;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLabNum(int labNum) {
+        this.labNum = labNum;
+    }
+
+    public Lab[] getLabs() {
+        return labs;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Student getMaster() {
+        return master;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLabNum() {
+        return labNum;
+    }
 }
