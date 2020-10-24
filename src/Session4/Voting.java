36f482caab8cd653869116eed9d786b0de104c14
Sajjad Pishvaeian <pishvaeian@aut.ac.ir>
2020-10-24 21:54:23 +0330

    methods created for Voting and VotingSystem class.

--- src/Session4/Voting.java
+++ src/Session4/Voting.java
@@ -1,4 +1,42 @@
 package Session4;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Voting {
+
+    private int type;
+    private String question;
+    private ArrayList<Person> voters;
+    private HashMap<String, HashSet<Vote>> polls;
+
+    public Voting(int type, String question) {
+        this.type = type;
+        this.question = question;
+    }
+
+    public void createPoll(String question){
+
+    }
+
+    public void vote(Person person,ArrayList<String> q){
+
+    }
+
+    public void getVoters(){
+
+    }
+
+    public void printVotes(){
+
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public HashMap<String, HashSet<Vote>> getPolls() {
+        return polls;
+    }
 }
--- src/Session4/VotingSystem.java
+++ src/Session4/VotingSystem.java
@@ -1,4 +1,32 @@
 package Session4;
 
+import java.util.ArrayList;
+
 public class VotingSystem {
+
+    private ArrayList<Voting> votingList;
+
+    public VotingSystem(){
+
+    }
+
+    public void createVoting(String question,int type,ArrayList<String> list){
+
+    }
+
+    public void getVotingList(){
+
+    }
+
+    public void getVoting(int type){
+
+    }
+
+    public void Vote(int num,Person voter,ArrayList<String> votes){
+
+    }
+
+    public void getResults(int num){
+
+    }
 }
