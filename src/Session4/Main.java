818a4053e77c03abee65d3a51df85ca47bce5542
Sajjad Pishvaeian <pishvaeian@aut.ac.ir>
2020-10-24 13:48:06 +0330

    Person class and Vote Class methods implemented. Jalali Calendar tested.

--- src/Session4/Main.java
+++ src/Session4/Main.java
@@ -1,7 +1,19 @@
 package Session4;
 
+import ir.huri.jcal.JalaliCalendar;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.GregorianCalendar;
+
 public class Main {
     public static void main(String[] args) {
-
+//        System.out.println(java.time.LocalDate.now());
+//        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        System.out.println(java.time.LocalDate.now());
+//        ZonedDateTime date = gregorianCalendar.toZonedDateTime();
+//        LocalDate date = LocalDate.now();
+        JalaliCalendar jalaliCalendar = new JalaliCalendar();
+        System.out.println(jalaliCalendar.toString());
     }
 }
