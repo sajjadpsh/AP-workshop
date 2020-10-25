81dcc3f3bf85c2d85dc4b80edf792d54bae269af
Sajjad Pishvaeian <pishvaeian@aut.ac.ir>
2020-10-25 23:00:58 +0330

    methods of VotingSystem class implemented.

--- src/Session4/VotingSystem.java
+++ src/Session4/VotingSystem.java
@@ -6,27 +6,59 @@ public class VotingSystem {
 
     private ArrayList<Voting> votingList;
 
-    public VotingSystem(){
+    public VotingSystem() {
+        votingList = new ArrayList<>();
+    }
+
+    public void createVoting(String question, int type, ArrayList<String> list) {
+        Voting voting = new Voting(type, question);
+        for (String temp: list) {
+            voting.createPoll(temp);
+        }
+        votingList.add(voting);
+        System.out.println("voting created.");
+    }
+
+    public void getVotingList() {
+        for (Voting temp: votingList) {
+            System.out.println(temp.getQuestion());
+        }
+    }
+
+    public void getVoting(int i) {
+        if (validIndex(i))
+        {
+            System.out.println("Question: "+ votingList.get(i).getQuestion());
+            ArrayList<String> temp = votingList.get(i).getPolls();
+            for (String move: temp)
+            {
+                System.out.println("\t"+move);
+            }
+        }
 
     }
 
-    public void createVoting(String question,int type,ArrayList<String> list){
-
+    public void Vote(int i, Person voter, ArrayList<String> votes) {
+        if(validIndex(i)) {
+            votingList.get(i).vote(voter, votes);
+        }
     }
 
-    public void getVotingList(){
-
+    public void getResults(int i){
+        if(validIndex(i)) {
+            System.out.println("print result:\tvoters:");
+            votingList.get(i).getVoters();
+            System.out.println("\t\tresult:");
+            votingList.get(i).printVotes();
+        }
     }
 
-    public void getVoting(int type){
-
-    }
-
-    public void Vote(int num,Person voter,ArrayList<String> votes){
-
-    }
-
-    public void getResults(int num){
-
+    private boolean validIndex(int index) {
+        if (index < 0 || index >= votingList.size()) {
+            System.out.println("Index is invalid");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
