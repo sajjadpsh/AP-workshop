67a9a537d6ad2b851c4696884eb1126d36201fd5
Sajjad Pishvaeian <pishvaeian@aut.ac.ir>
2020-11-13 10:49:13 +0330

    Calculator frame created.standard and scientific calculator panels added.

--- src/Session7/AP_Calculator.java
+++ src/Session7/AP_Calculator.java
@@ -1,6 +1,37 @@
 package Session7;
 
 
-public class AP_Calculator {
+import javax.swing.*;
 
+public class AP_Calculator extends JFrame {
+
+    //main frame
+    private JFrame calc_Frame;
+    //standard calculator panel
+    private JPanel standardCalc;
+    //scientific calculator panel
+    private JPanel scientificCalc;
+
+    /**
+     * Create a Calculator.
+     */
+    public AP_Calculator() {
+
+        //frame
+        calc_Frame = new JFrame();
+        calc_Frame.setTitle("AUT Calculator");
+        calc_Frame.setSize(400, 600);
+        calc_Frame.setLocation(100, 200);
+        calc_Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //tab
+        JTabbedPane calculatorTabs = new JTabbedPane();
+        calculatorTabs.addTab("Standard ", standardCalc);
+        calculatorTabs.addTab("Scientific ", scientificCalc);
+        calc_Frame.add(calculatorTabs);
+        calc_Frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new AP_Calculator();
+    }
 }
