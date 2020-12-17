6b87e416f600ba538301f721c73a6da2a431e78a
Sajjad Pishvaeian <pishvaeian@aut.ac.ir>
2020-12-17 17:38:51 +0330

    Session 9 initiated.

---
+++ src/Session9/Main.java
@@ -0,0 +1,16 @@
+package Session9;
+
+import Session9.gui.CFrame;
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CFrame frame = new CFrame("iNote");
+        frame.setVisible(true);
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+}
